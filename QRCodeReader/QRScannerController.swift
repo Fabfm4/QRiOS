3e00a47497796b5dcfd36dd348c3e09ec3dbded5
Fabfm4 <FabVinco@VincoOrbiss-MacBook-Air.local>
2016-11-29 18:36:18 -0600

    init

---
+++ QRCodeReader/QRScannerController.swift
@@ -0,0 +1,96 @@
+//
+//  QRScannerController.swift
+//  QRCodeReader
+//
+//  Created by Simon Ng on 13/10/2016.
+//  Copyright © 2016 AppCoda. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class QRScannerController: UIViewController,AVCaptureMetadataOutputObjectsDelegate {
+
+    @IBOutlet var messageLabel:UILabel!
+    @IBOutlet var topbar: UIView!
+    
+    var captureSession:AVCaptureSession?
+    var videoPreviewLayer:AVCaptureVideoPreviewLayer?
+    var qrCodeFrameView:UIView?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let captureDevice = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeVideo)
+        do{
+            let input  = try AVCaptureDeviceInput(device: captureDevice)
+            
+            captureSession = AVCaptureSession()
+            
+            captureSession?.addInput(input)
+            
+            let captureMediadataOutput = AVCaptureMetadataOutput()
+            captureSession?.addOutput(captureMediadataOutput)
+            
+            captureMediadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+            captureMediadataOutput.metadataObjectTypes = [AVMetadataObjectTypeQRCode]
+            
+            videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+            videoPreviewLayer?.videoGravity = AVLayerVideoGravityResizeAspectFill
+            videoPreviewLayer?.frame = view.layer.bounds
+            view.layer.addSublayer(videoPreviewLayer!)
+            captureSession?.startRunning()
+            
+            view.bringSubview(toFront: messageLabel)
+            view.bringSubview(toFront: topbar)
+            
+            qrCodeFrameView = UIView()
+            if let qrCodeFrameView = qrCodeFrameView {
+                qrCodeFrameView.layer.borderColor = UIColor.green.cgColor
+                qrCodeFrameView.layer.borderWidth = 2
+                view.addSubview(qrCodeFrameView)
+                view.bringSubview(toFront: qrCodeFrameView)
+            }
+        } catch {
+            print(error)
+            return
+        }
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    
+    func captureOutput(_ captureOutput: AVCaptureOutput!, didOutputMetadataObjects metadataObjects: [Any]!, from connection: AVCaptureConnection!) {
+        if metadataObjects == nil || metadataObjects.count == 0 {
+            qrCodeFrameView?.frame = CGRect.zero
+            messageLabel.text = "No QE code is detected"
+            return
+        }
+        
+        let metadataObj = metadataObjects[0] as! AVMetadataMachineReadableCodeObject
+        
+        if metadataObj.type == AVMetadataObjectTypeQRCode {
+            let barCodeObject = videoPreviewLayer?.transformedMetadataObject(for: metadataObj)
+            qrCodeFrameView?.frame = barCodeObject!.bounds
+            if metadataObj.stringValue != nil {
+                messageLabel.text = metadataObj.stringValue
+            }
+        }
+    }
+
+}
